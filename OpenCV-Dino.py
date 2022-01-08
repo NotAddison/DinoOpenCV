daf736c95fb2f2ff16db839fad301319406ea71c
NotAddison <S10222525@connect.np.edu.sg>
2022-01-08 11:26:29 +0800

    Added Screencap

--- OpenCV-Dino.py
+++ OpenCV-Dino.py
@@ -8,3 +8,21 @@ from PIL import Image, ImageGrab
 
 #Keyboard Emulation
 import pyautogui as pag
+
+while True:
+    # ScreenGrabbing
+    img = ImageGrab.grab(bbox=(400,400,800,800)) #bbox default = whole screen | (left_x, top_y, right_x, bottom_y)
+
+    img_arry = np.array(img) # Convert Image to an array
+    img_grey = cv2.cvtColor(img_arry, cv2.COLOR_BGR2GRAY) # Convert to greyscale
+    cv2.imshow("",img_grey) # Show screencaptured images (videos = buch of photos (frames))
+
+    # Compare Images
+    template = cv2.imread(r'.\template\catc1.png',0) #0 parameter = read image in greyscale mode
+
+    # Keyboard Input
+
+
+    if cv2.waitKey(1) == 27: break #Keybaord key ID ; 27 = esc
+
+cv2.destroyAllWindows()
